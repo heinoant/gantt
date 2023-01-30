7492254ddb21b8b4e22b394897c77efe446d684a
heinoant <40440729+heinoant@users.noreply.github.com>
2023-01-30 10:21:31 +0200

    Render more dates

--- src/index.js
+++ src/index.js
@@ -229,14 +229,18 @@ export default class Gantt {
             this.gantt_start = date_utils.add(this.gantt_start, -7, 'day');
             this.gantt_end = date_utils.add(this.gantt_end, 7, 'day');
         } else if (this.view_is(VIEW_MODE.MONTH)) {
-            this.gantt_start = date_utils.start_of(this.gantt_start, 'year');
-            this.gantt_end = date_utils.add(this.gantt_end, 1, 'year');
+            this.gantt_start = date_utils.start_of(
+                this.gantt_start,
+                -2,
+                'year'
+            );
+            this.gantt_end = date_utils.add(this.gantt_end, 2, 'year');
         } else if (this.view_is(VIEW_MODE.YEAR)) {
             this.gantt_start = date_utils.add(this.gantt_start, -2, 'year');
             this.gantt_end = date_utils.add(this.gantt_end, 2, 'year');
         } else {
-            this.gantt_start = date_utils.add(this.gantt_start, -1, 'month');
-            this.gantt_end = date_utils.add(this.gantt_end, 1, 'month');
+            this.gantt_start = date_utils.add(this.gantt_start, -2, 'month');
+            this.gantt_end = date_utils.add(this.gantt_end, 2, 'month');
         }
     }
 
